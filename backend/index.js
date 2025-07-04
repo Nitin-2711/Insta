d11cdfddc58bb12b18aa7e55c831013ead3fa9a6
Nitin Kumar <104916021+Nitin-2711@users.noreply.github.com>
2025-07-04 14:15:53 +0530

    signup data saved complete

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const {mongoClient} = require("mongodb");
-const jwt = require("jsonwebtoken");const app = require();
-
-app.use(express);
-
+const jwt = require("jsonwebtoken");
 const app = express();
+
+app.use(express.json());
 app.post("signup", async(req, res) => {
     let client=new mongoClient("mongodb://localhost:27017/"); 
-    let collection =db.collection("users");
-     await collection.insertOne(req.body);
-     res.json({"message": "User signed up successfully!"});
-
-})
+    await client.connect();
+    let db = client.db("Insta");
+    let collection = db.collection("users");
+    await collection.insertOne(req.body);
+    res.json({"Message":'Data saved'}); (err, result) => {
+});
