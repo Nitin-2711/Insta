57b80af5dbbe850bd83fc72035b5dd78673ccdc1
Nitin Kumar <104916021+Nitin-2711@users.noreply.github.com>
2025-07-04 14:08:58 +0530

    start

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const {mongoClient} = require("mongodb");
-const jwt = require('jsonwebtoken');
-const e = require('cors');
+const jwt = require("jsonwebtoken");const app = require();
 
 app.use(express);
 
 const app = express();
 app.post("signup", async(req, res) => {
-    let client=new mongoClient(); 
+    let client=new mongoClient("mongodb://localhost:27017/"); 
+    let collection =db.collection("users");
+     await collection.insertOne(req.body);
+     res.json({"message": "User signed up successfully!"});
+
 })
