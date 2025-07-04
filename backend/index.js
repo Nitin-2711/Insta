56b7b553ce30369b7ad47d8c80eeae11322cad3a
Nitin Kumar <104916021+Nitin-2711@users.noreply.github.com>
2025-07-04 14:27:03 +0530

    under dev

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+
 const {mongoClient} = require("mongodb");
 const jwt = require("jsonwebtoken");
 const app = express();
@@ -11,5 +11,22 @@ app.post("signup", async(req, res) => {
     let db = client.db("Insta");
     let collection = db.collection("users");
     await collection.insertOne(req.body);
+    res.json({"Message":'Data saved'}); (err, result) => {
+});
+
+app.post("Login", async(req, res) => {
+    let client=new mongoClient("mongodb://localhost:27017/"); 
+    await client.connect();
+    let db = client.db("Insta");
+    let collection = db.collection("users");
+    let email = req.body.email;
+    let password = req.body.password;
+
+    let row=await collection.findOne({"email": email});
+    if(row == null) {
+        res.json({"Message":'User not found'});
+       return;
+    }
+    
     res.json({"Message":'Data saved'}); (err, result) => {
 });
